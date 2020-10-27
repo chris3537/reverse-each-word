69971e4de68ef948a4d5d8f459a49f3e6aca63b9
chris3537 <chrisbhs3537@gmail.com>
2020-10-27 17:12:50 -0400

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+
+
+def reverse_each_word(sentence)
+ array_sentence = sentence.split(" ")
+        
+ array_reverse = array_sentence.collect {|word| word.reverse}
+ array_reverse.join(" ")
+        
+        
+        
+ end
